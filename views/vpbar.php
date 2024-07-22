50d654451ef38efbd52bc906937d06a80dedea66
Dilan <dilan8109@gmail.com>
2024-07-21 19:48:48 -0500

    1.1

--- views/vpbar.php
+++ views/vpbar.php
@@ -1,11 +1,11 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="style.css">
-<div class="container mt-3 mb-5">
-    <h1 class="mb-4">Datos personales</h1>
-    <form action="#" method="post">
+
+<div id="perfil" class="container mt-3 mb-5">
+    <form action="#" method="post" enctype="multipart/form-data"></form>
         <div class="row">
+        <h2 class="fw-bold mb-3"><i class="fa-solid fa-file-shield"></i>Datos personales</h2>
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="name">Nombre del bar:</label>
