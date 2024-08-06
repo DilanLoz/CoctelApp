a6f2e6f779661bf466b0bfc1382c6924f1405704
Dilan <dilan8109@gmail.com>
2024-08-06 10:10:42 -0500

    2.4

--- views/vemhipe.php
+++ views/vemhipe.php
@@ -1,12 +1,14 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/inho.css">
 
 <div class="container mt-5 mb-5">
-    <h1 class="text-center">Historial de pedidos</h1>
+    <h1 class="text-center mb-5">Historial de pedidos<div class="text-center col-4 border border-warning border-3 rounded-3 mt-4 mx-auto"></div>
+    </h1>
+    
     <div class="row">
-        <div class="col marg">ID pedido
+        <div class="col marg"> <h6 class="fw-bold">ID Del Pedido</h6>
             <div>
                 <ul class="list-unstyled">
                     <li class="border border-warning rounded-3 ps-2">ID 2021</li>
@@ -14,7 +16,7 @@
                 </ul>
             </div>
         </div>
-        <div class="col marg">Fecha y hora
+        <div class="col marg"><h6 class="fw-bold">Fecha y Hora</h6>
             <div>
                 <ul class="list-unstyled" id="ushipe">
                     <li class="border border-warning rounded-3 ps-2">Jul 03, 11:31 AM</li>
@@ -22,7 +24,7 @@
                 </ul>
             </div>
         </div>
-        <div class="col marg" id="estped">Estado
+        <div class="col marg" id="estped"><h6 class="fw-bold">Estado Del Pedido</h6>
             <div>
                 <ul class="list-unstyled" id="ushipe">
                     <li class="bg-black text-white ps-2 border border-black rounded-3">
