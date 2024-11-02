2104b0789831356a95ffec05a027b6842fb582cb
Dilan <dilan8109@gmail.com>
2024-11-02 16:30:02 -0500

    css para productos

--- views/vusvino.php
+++ views/vusvino.php
@@ -1,10 +1,7 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
 <link rel="stylesheet" href="css/carcomp.css">
 
-<section class="shop container">
+<section class="shop container" style="text-align: left; display: block;">
     <h2 class="section-title"><i class="fa-solid fa-wine-glass"></i> Vinos</h2>
     <div class="shop-content">
         <div class="product-box">
