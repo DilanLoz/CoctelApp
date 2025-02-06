5c56d45e786e1da8d68b53ee74b1c14b5113df32
DilanLoz <dilan8109@gmail.com>
2025-02-06 16:21:54 -0500

    vista carrito

--- views/vuscarcom.php
+++ views/vuscarcom.php
@@ -1,61 +1,181 @@
-
 <div class="container mt-5 col-12 col-md-8" id="detpedpq" style="text-align: left">
     <div class="mt-4">
-        <h1 class="text-warning fw-bold" style="font-size: 30px"><i class="fa-solid fa-cart-shopping"></i> Carrito</h1>
+        <h1 class="text-warning fw-bold" style="font-size: 30px">
+            <i class="fa-solid fa-cart-shopping"></i> Carrito
+        </h1>
         <div class="row">
-            <div class="container rounded-3 me-4 mb-5 text-black" style="background-color: #fff;">
-                
+            <div class="rounded-3 me-4 mb-5 text-black" style="background-color: #fff;">
                 <!-- Producto 1 -->
-                <div class="row product-item pt-3 ps-3 pe-3 mt-2 rounded rounded-3 border border-secondary mb-3">
-                    <div class="col-3 col-sm-2 d-flex justify-content-center">
-                        <img src="img/coctelapp/logo.png" alt="" class="prodcar" draggable="false" style="width: 50px; height: 70px;">
+                <div class="rounded-3 border border-secondary mb-3">
+                    <div class="row product-item pt-3 ps-3 pe-3 mt-3 bg-body-tertiary rounded-3 m-4">
+                        <!-- Imagen del producto -->
+                        <div class="col-3 col-sm-2 d-flex justify-content-center">
+                            <img src="img/coctelapp/logo.png" alt="" class="prodcar" draggable="false" style="width: 50px; height: 70px;">
+                        </div>
+
+                        <!-- Información del producto -->
+                        <div class="col-6 col-sm-6">
+                            <ul style="list-style-type: none; padding-left: 0;">
+                                <li><b>Licor Sake Yaegaki</b></li>
+                                <li><b style="font-size: 13px;">Bar Rocas</b></li>
+                                <li><b class="text-warning">190.000</b></li>
+                            </ul>
+                        </div>
+
+                        <!-- Contenedor del contador y botón de eliminar -->
+                        <div class="col-3 d-flex justify-content-between align-items-center mb-3">
+                            <!-- Contador -->
+                            <div class="counter d-flex align-items-center">
+                                <button class="btn btn-outline-success btn-sm increment">
+                                    <i class="fa-solid fa-plus"></i>
+                                </button>
+
+                                <input type="text" class="text-center mx-1 form-control quantity" value="1" readonly>
+
+                                <button class="btn btn-outline-danger btn-sm decrement">
+                                    <i class="fa-solid fa-minus"></i>
+                                </button>
+                            </div>
+
+                            <!-- Botón de eliminar -->
+                            <button class="trash mb-2">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </div>
                     </div>
-                    <div class="col-7 col-sm-8">
-                        <ul style="list-style-type: none; padding-left: 0;">
-                            <li><b>Licor Sake Yaegaki</b></li>
-                            <li><b style="font-size: 13px;">750ml</b></li>
-                            <li><b class="text-warning">190.000</b></li>
-                        </ul>
-                    </div>
-                    <div class="col-2 d-flex flex-column align-items-center justify-content-center">
-                        <button class="trash" style="background-color: transparent; border-color: transparent; padding: 0;">
-                            <i class="fa-solid fa-trash" style="color: #f00000;"></i>
-                        </button>
-                        <select class="selcprod" style="margin-top: 10px; width: 70px;" class=" form-control form-select">
-                            <option selected>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                            <option>7</option>
-                            <option>8</option>
-                            <option>9</option>
-                            <option>10</option>
-                        </select>
+                    <div class="row product-item pt-3 ps-3 pe-3 mt-3 bg-body-tertiary rounded-3 m-4">
+                        <!-- Imagen del producto -->
+                        <div class="col-3 col-sm-2 d-flex justify-content-center">
+                            <img src="img/coctelapp/logo.png" alt="" class="prodcar" draggable="false" style="width: 50px; height: 70px;">
+                        </div>
+
+                        <!-- Información del producto -->
+                        <div class="col-6 col-sm-6">
+                            <ul style="list-style-type: none; padding-left: 0;">
+                                <li><b>Licor Sake Yaegaki</b></li>
+                                <li><b style="font-size: 13px;">Cabañas</b></li>
+                                <li><b class="text-warning">190.000</b></li>
+                            </ul>
+                        </div>
+
+                        <!-- Contenedor del contador y botón de eliminar -->
+                        <div class="col-3 d-flex justify-content-between align-items-center mb-3">
+                            <!-- Contador -->
+                            <div class="counter d-flex align-items-center">
+                                <button class="btn btn-outline-success btn-sm increment">
+                                    <i class="fa-solid fa-plus"></i>
+                                </button>
+
+                                <input type="text" class="text-center mx-1 form-control quantity" value="1" readonly>
+
+                                <button class="btn btn-outline-danger btn-sm decrement">
+                                    <i class="fa-solid fa-minus"></i>
+                                </button>
+                            </div>
+
+                            <!-- Botón de eliminar -->
+                            <button class="trash mb-2">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
 
                 <!-- Cálculos y pagos -->
-                <div class="mb-3 mt-3">
+                <div class="mb-5 mt-3">
+                    <div class="row mt-2">
+                        <label class="col-6 col-md-6 col-form-label"><strong>Costo del envío</strong></label>
+                        <label class="costenvio col-6 col-md-6 text-end p-2"><strong>$ 4.200</strong></label>
+                    </div>
                     <div class="row">
-                        <label class="col-7 col-md-9 col-form-label mt-2"><strong>Código de descuento</strong></label>
-                        <input type="text" class="border-3 rounded border border-warning col-5 col-md-3 mt-3 text-end">
-                    </div>
-                    <div class="row mt-2">
-                        <label class= col-6 col-md-4 col-form-label"><strong>Costo del envío</strong></label>
-                        <label class="costenvio col-6 col-md-4 text-end p-2"><strong>$ 4.200</strong></label>
-                    </div>
-                    <div class="border rounded border-warning border-2 mt-2"></div>
-                    <div class="row mt-2">
-                        <label class="col-6 col-md-6 col-form-label"><strong>Total a pagar</strong></label>
-                        <label class="col-6 col-md-6 text-end"><strong>$ 528.990</strong></label>
+                        <label class="col-6 col-md-6 col-form-label"><strong>Costo Total</strong></label>
+                        <label class="costenvio col-6 col-md-6 text-end p-2"><strong>$ 44.800</strong></label>
                     </div>
                     <div class="text-end mt-3">
-                        <a href="home.php?pg=1012" class="btn btn-warning col-12 col-md-6">Ir a pagar</a>
+                        <a href="home.php?pg=1012" class="btn btn-warning col-12 col-md-2">Ir a pagar</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    document.querySelectorAll('.increment').forEach(button => {
+        button.addEventListener('click', function() {
+            let input = this.nextElementSibling;
+            let valor = parseInt(input.value, 10);
+            if (valor < 10) {
+                input.value = valor + 1;
+            }
+        });
+    });
+
+    document.querySelectorAll('.decrement').forEach(button => {
+        button.addEventListener('click', function() {
+            let input = this.previousElementSibling;
+            let valor = parseInt(input.value, 10);
+            if (valor > 1) {
+                input.value = valor - 1;
+            }
+        });
+    });
+</script>
+
+<style>
+    /* 🔹 Contenedor del contador y botón de eliminar */
+    .counter {
+        display: flex;
+        align-items: center;
+        gap: 5px;
+    }
+
+    /* 🔹 Botón de eliminar */
+    .trash {
+        background-color: transparent;
+        border: none;
+        color: red;
+        font-size: 18px;
+        cursor: pointer;
+    }
+
+    /* 🔹 Input del contador */
+    .counter input {
+        width: 45px;
+        font-size: 14px;
+        text-align: center;
+        padding: 5px;
+        margin: 0 5px;
+    }
+
+    /* 🔹 Solo para móviles (menos de 768px) */
+    @media (max-width: 768px) {
+        .counter {
+            flex-direction: column;
+        }
+
+        .counter input {
+            width: 35px;
+            font-size: 12px;
+        }
+
+        .counter button {
+            width: 30px;
+            height: 30px;
+            font-size: 12px;
+            padding: 3px;
+        }
+
+        /* 🔹 Ajusta el contenedor en móviles */
+        .col-3.d-flex {
+            flex-direction: column-reverse;
+            align-items: center;
+            justify-content: center;
+        }
+
+        /* 🔹 El botón de eliminar se mantiene alineado a la derecha */
+        .trash {
+            margin-top: 5px;
+        }
+    }
+</style>
